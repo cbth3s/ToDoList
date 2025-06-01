3dc4f7ee4376963c2bcc8dc0fe7a5e08934bfef9
Ukhvatov Leonid <leonid.ukhvatov@avbinvest.co>
2025-06-01 18:33:58 +0300

    Create develop branch

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by name surname on 01.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
